32dcffbc21137010592b45f19d8965bea0fdb76c
remusRD <remusRichardDumitrache@gmail.com>
2018-07-23 21:56:51 +0200

    Adding sendMails test

--- src/main/java/TestClass.java
+++
@@ -1,5 +0,0 @@
-public class TestClass {
-    public void nothing(){
-        System.out.println("yo");
-    }
-}
--- src/test/java/Test.java
+++
@@ -1,6 +0,0 @@
-public class Test {
-    @org.junit.jupiter.api.Test
-    public void nothing(){
-
-    }
-}
